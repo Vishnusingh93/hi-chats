cd2551e1dfd9a5e7b68733f0d96c73bc28519fc6
Vishnu Singh <vishnusingh93524@gmail.com>
2025-11-24 19:52:02 +0530

    Refactor appwriteConfig.js for client setup
    Refactor appwriteConfig.js to uncomment and reorganize code for client initialization and exports.

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,22 +1,40 @@
-import { Client, Databases, Account,Storage } from 'appwrite';
+// import { Client, Databases, Account,Storage } from 'appwrite';
 
 
-export const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+// export const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+// export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+// export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_APPWRITE_COLLECTION_ID_MESSAGES;
+// export const BUCKET_ID = import.meta.env.VITE_APPWRITE_BUCKET_ID;
+
+
+
+// const client = new Client();
+// client
+
+//     .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+//     .setProject(PROJECT_ID);
+
+//     export const databases = new Databases(client);
+//     export const account = new Account(client);
+//     export const storage = new Storage(client);
+    
+
+//     export default client;
+
+import { Client, Databases, Account, Storage } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+export const databases = new Databases(client);
+export const account = new Account(client);
+export const storage = new Storage(client);
+
 export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_APPWRITE_COLLECTION_ID_MESSAGES;
 export const BUCKET_ID = import.meta.env.VITE_APPWRITE_BUCKET_ID;
 
-
-
-const client = new Client();
-client
-
-    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(PROJECT_ID);
-
-    export const databases = new Databases(client);
-    export const account = new Account(client);
-    export const storage = new Storage(client);
-    
-
-    export default client;
+export default client;
